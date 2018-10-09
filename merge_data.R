41f54194025057ace0b103a4df70160fc35b064b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-09 10:25:07 -0400

    Saving merge_data.R

--- merge_data.R
+++ merge_data.R
@@ -1,6 +1,6 @@
 
-cleandata2 <- USArrests
-clean_data2 <- c(USArrests,rownames(USArrests))
+#cleandata2 <- USArrests
+x <- rownames(USArrests)
 #clean_data_merged <- merge(clean_data,cleandata2,by.x=clean_data$statename,by.y=rownames(USArrests))
 
 #str(clean_data_merged)
