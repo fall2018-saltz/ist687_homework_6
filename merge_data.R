421c75c56f71432dbd10de75d979b6aa96a8c39e
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 14:33:33 -0400

    Saving merge_data.R

--- merge_data.R
+++ merge_data.R
@@ -1,7 +1,7 @@
 
 cleandata2 <- USArrests
 
-#clean_data_merged <- merge(clean_data,cleandata2,by="row.names")
+
 # save row names as a separate variable called stateName
 x <- as.vector(rownames(USArrests))
 
