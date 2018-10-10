1da625117ef08bef1edf3d903339846f9ee21c86
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 13:05:24 -0400

    Saving histogram1.R

--- histogram1.R
+++ histogram1.R
@@ -1,6 +1,6 @@
 
 library("ggplot2")
 
-ggplot(clean_data_merged, aes(x=clean_data_merged$population)) + 
+x <- ggplot(clean_data_merged, aes(x=clean_data_merged$population)) + 
   geom_histogram(color="blue", binwidth=50)
-
+x
