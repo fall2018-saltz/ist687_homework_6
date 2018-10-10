779fa099cff84cddd26f945c96f5584eeefb7406
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 14:22:10 -0400

    Saving scatter_plot.R

--- scatter_plot.R
+++ scatter_plot.R
@@ -1,1 +1,6 @@
 
+library("ggplot2")
+scatterplot <- ggplot(murders, aes(x= population, y =popOver18))
+scatterplot <- scatterplot + geom_point(aes(size = numMurders, color = numMurders))
+scatterplot <- scatterplot + ggtitle("Scatter Plot")
+scatterplot
