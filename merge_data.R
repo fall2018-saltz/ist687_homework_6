691add27e305a8446cd71405ddcb49b69346ed18
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 14:32:55 -0400

    Saving merge_data.R

--- merge_data.R
+++ merge_data.R
@@ -4,6 +4,8 @@ cleandata2 <- USArrests
 #clean_data_merged <- merge(clean_data,cleandata2,by="row.names")
 # save row names as a separate variable called stateName
 x <- as.vector(rownames(USArrests))
+
+# add this column to the dataframe
 cleandata2$stateName <- x
 clean_data_merged <- merge(clean_data,cleandata2,by="stateName")
 head(clean_data_merged)
