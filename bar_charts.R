aa87f2500423286616f7f4971c751c8f1037db46
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 15:22:39 -0400

    Saving bar_charts.R

--- bar_charts.R
+++ bar_charts.R
@@ -22,14 +22,15 @@ barChart2 <- barChart2 + ggtitle("Total Murders")
 barChart2 <- barChart2 + theme(axis.text.x = element_text(angle = 90, hjust = 1))
 barChart2
 
-#Generate a new bar chart, the same as in the previous step, 
+# Generate a new bar chart, the same as in the previous step, 
 # but also sort the x-axis by the murder rate
 barChart3 <- ggplot(clean_data_merged, aes(x = reorder(stateName, murdersperstate), y = murdersperstate, group = 1))
 barChart3 <- barChart3 + geom_col()
 barChart3 <- barChart3 + ggtitle("Sorted bar chart of murders per state")
 barChart3
 
-Generate a third bar chart, the same as the previous stepbut also showing percentOver18 as the color of the bar
+# Generate a third bar chart, the same as the previous step
+# but also showing percentOver18 as the color of the bar
 barChart4 <- ggplot(clean_data_merged, aes(x = reorder(stateName, murdersperstate), y = murdersperstate, group = 1))
 barChart4 <- barChart4 + geom_col(aes(size=percentOver18,color=percentOver18))
 barChart4 <- barChart4 + ggtitle("Sorted bar chart showing percentOver18 as color of bar")
