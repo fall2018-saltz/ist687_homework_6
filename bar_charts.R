9e30905138c06bc051c23f565ddde5b5eb1ae90c
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 14:14:21 -0400

    Saving bar_charts.R

--- bar_charts.R
+++ bar_charts.R
@@ -1,7 +1,7 @@
 
 library("ggplot2")
 clean_data_merged$murdersperstate <- as.integer(clean_data_merged$population*clean_data_merged$Murder/100000)
- clean_data_mergedmurders <- clean_data_merged[,c(1,2,3,9)]
+murders <- clean_data_merged[,c(1,2,3,9)]
 murders
 
 barChart1 <- ggplot(clean_data_merged, aes(x=stateName, y=murdersperstate))
