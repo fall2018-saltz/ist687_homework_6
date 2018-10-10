33480afee1cf8adf92d53547d4bb2d4ef8abd0af
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 14:33:24 -0400

    Saving merge_data.R

--- merge_data.R
+++ merge_data.R
@@ -7,5 +7,7 @@ x <- as.vector(rownames(USArrests))
 
 # add this column to the dataframe
 cleandata2$stateName <- x
+
+# merge the dataframes based on the stateName column in both the dataframes
 clean_data_merged <- merge(clean_data,cleandata2,by="stateName")
 head(clean_data_merged)
