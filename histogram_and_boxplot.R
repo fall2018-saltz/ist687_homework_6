e0097755a4fbc255dd646264fc284accd72bc807
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 14:38:23 -0400

    Saving histogram_and_boxplot.R

--- histogram_and_boxplot.R
+++ histogram_and_boxplot.R
@@ -12,6 +12,7 @@ myPlotPop <- myPlotPop + geom_histogram(binwidth=500000)
 myPlotPop <- myPlotPop + ggtitle("Histogram of Population")
 myPlotPop
 
+# murder
 myPlotPop1 <- ggplot(clean_data_merged, aes(x=Murder))
 myPlotPop1 <- myPlotPop1 + geom_histogram(binwidth=1)
 myPlotPop1 <- myPlotPop1 + ggtitle("Histogram of Murder")
