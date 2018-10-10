9ccddbdd7f4fd175bba8a05b0f982f2e3a28d4cf
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 14:35:37 -0400

    Saving histogram_and_boxplot.R

--- histogram_and_boxplot.R
+++ histogram_and_boxplot.R
@@ -2,6 +2,7 @@
 library("ggplot2")
 
 # ggplot is used to create plots
+# the geom function is used to choose which plot to make
 myPlotPop <- ggplot(clean_data_merged, aes(x=population))
 myPlotPop <- myPlotPop + geom_histogram(binwidth=500000)
 myPlotPop <- myPlotPop + ggtitle("Histogram of Population")
