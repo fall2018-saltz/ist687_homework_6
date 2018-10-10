41c945ce0e88c4f465ad8bc26c0fc365f0cf089c
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 13:14:56 -0400

    Saving histogram1.R

--- histogram1.R
+++ histogram1.R
@@ -2,11 +2,11 @@
 library("ggplot2")
 
 myPlotPop <- ggplot(clean_data_merged, aes(x=population))
-myPlotPop <- myPlotPop + geom_histogram()
+myPlotPop <- myPlotPop + geom_histogram(binwidth=50)
 myPlotPop <- myPlotPop + ggtitle("Histogram of Population")
 myPlotPop
 
 myPlotPop1 <- ggplot(clean_data_merged, aes(x=Murder))
-myPlotPop1 <- myPlotPop1 + geom_histogram()
+myPlotPop1 <- myPlotPop1 + geom_histogram(binwidth=50)
 myPlotPop1 <- myPlotPop1 + ggtitle("Histogram of Murder")
 myPlotPop1
